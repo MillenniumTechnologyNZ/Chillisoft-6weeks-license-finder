ea1bdf598fa0a2a3f09ce95302dd3b6833dc187f
tree_house@hotmail.co.nz <G@2uA$3v2Ty>
2021-08-20 12:16:34 +1200

    Added random variations of the xpaths to cope with different html

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # This is a sample Python script.
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options  # for suppressing the browser
-import secrets
 import pyautogui
-import numpy
+import pyperclip
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+import secrets
 
 myUrl = "https://miltech.repairshopr.com/invoices"
-
 
 
 def run():
@@ -18,11 +16,11 @@ def run():
 
 
 def test():
+    browser = setup()
     number = pyautogui.prompt("Click OK if you have copied the number to clipboard")
-    while number is not None or number is not "":
-        values = setup(number)
-        enter_values(values)
-        number = pyautogui.prompt("Click OK if you have copied the number to clipboard")
+    while number is not None or number != "":
+        values = main_run(browser, number)
+        number = enter_values(values)
 
 
 def enter_values(values):
@@ -30,21 +28,44 @@ def enter_values(values):
     val1 = values[0]
     val2 = values[1]
     pyautogui.typewrite(val2)
+    pyautogui.press('enter')
     pyautogui.move(150, 0)
     pyautogui.click()
     pyautogui.typewrite(val1)
+    pyautogui.move(-225, 20)
+    pyautogui.doubleClick()
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.press('enter')
+    pyautogui.move(75, 0)
+    pyautogui.sleep(0.5)
+    return pyperclip.paste()
 
 
-
-def setup(number):
+def setup():
     option = webdriver.ChromeOptions()
     option.add_argument('headless')
+    #
     browser = webdriver.Chrome('chromedriver.exe', options=option)
-    type(browser)
+    return browser
+
+
+def main_run(browser, number):
     browser.get(myUrl)
-    login(browser)
-    search_num(browser, number)
+    is_logged_in(browser)
+    try:
+        search_num(browser, number)
+    except:
+        return ["N/A", "N/A"]
     return get_info(browser)
+
+
+def is_logged_in(browser):
+    try:
+        browser.find_element_by_id("user_email")
+        login(browser)
+    except:
+        return
 
 
 def search_num(browser, number):
@@ -56,17 +77,64 @@ def search_num(browser, number):
     my_elem = WebDriverWait(browser, 3).until(EC.presence_of_element_located(
         (By.XPATH, "//*[text()=\'" + number + "\']")))
     my_elem.click()
-    pyautogui.alert("Open invoice")
+    print("Opening invoice")
 
 
 def get_info(browser):
-    mobile = browser.find_element_by_xpath(
-        "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[6]/td/a")
-    print(mobile.get_attribute('text'))
-    name = browser.find_element_by_xpath(
-        "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[2]/td/span/a")
-    print(name.get_attribute('text'))
-    return [mobile.get_attribute('text'), name.get_attribute('text')]
+    try:
+        mobile = browser.find_element_by_xpath(
+            "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[6]/td/a").get_attribute('text')
+        print(mobile)
+    except:
+        try:
+            mobile = browser.find_element_by_xpath(
+                "/html/body/div[1]/div[3]/div/div/div[4]/div[1]/div[1]/div[2]/table/tbody/tr[6]/td/a").get_attribute(
+                'text')
+            print(mobile)
+        except:
+            mobile = "N/A"
+    try:
+        name = browser.find_element_by_xpath(
+            "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[2]/td/span/a")\
+            .get_attribute('text')
+
+        print(name)
+        if name == "(empty)":
+            name = browser.find_element_by_xpath(
+                "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[1]/td/span")\
+                .get_attribute('innerHTML').strip()
+            print(name)
+            print("Name is (empty)")
+            # get business name if name is empty
+    except:
+        try:
+            name = browser.find_element_by_xpath(
+                "/html/body/div[1]/div[3]/div/div/div[4]/div[1]/div[1]/div[2]/table/tbody/tr[2]/td/span/a") \
+                .get_attribute('text')
+
+            print(name)
+            if name == "(empty)":
+                name = browser.find_element_by_xpath(
+                    "/html/body/div[1]/div[3]/div/div/div[4]/div[1]/div[1]/div[2]/table/tbody/tr[1]/td/span") \
+                    .get_attribute('innerHTML').strip()
+                print(name)
+                print("Name is (empty)")
+        except:
+            try:
+                mobile = browser.find_element_by_xpath(
+                    "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[5]/td/a")\
+                    .get_attribute('innerHTML').strip()
+                print(mobile)
+                name = browser.find_element_by_xpath(
+                    "/html/body/div[1]/div[3]/div/div/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[1]/td/span/a")\
+                    .get_attribute('text')
+                print(name)
+            except:
+                name = "N/A"
+
+    print("sending info")
+    return [mobile, name]
+
 
 def login(browser):
     if browser.find_element_by_id("user_email"):
@@ -80,6 +148,7 @@ def login(browser):
         item.send_keys(secrets.password)
         submit_btn = browser.find_element_by_name("commit")
         submit_btn.click()
+    print("Signed in")
 
 
 # Press the green button in the gutter to run the script.
